0d982a3309b72d74ec7e29bfc71790a8bd6aaeee
airil <108640806+airil28@users.noreply.github.com>
2023-02-05 21:53:22 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+var cors = require('cors')
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// app.get('/dun', (req, res) => {
+//   res.sendFile(__dirname + '/2022-02/dun.json');
+// });
+
+app.get('/pilihanraya', (req, res) => {
+  res.sendFile(__dirname + '/pilihanraya.json');
+});
+
+app.get('/calondun', (req, res) => {
+  res.sendFile(__dirname + '/2022-02/calon_dun.json');
+});
+
+app.get('/calonparlimen', (req, res) => {
+  res.sendFile(__dirname + '/2022-02/calon_parlimen.json');
+});
+
+app.get('/parlimen', (req, res) => {
+  res.sendFile(__dirname + '/2022-02/parlimen.json');
+});
+
+app.get('/dun', (req, res) => {
+  res.sendFile(__dirname + '/2022-02/dun.json');
+});
+
+app.get('/peratusan', (req, res) => {
+  res.sendFile(__dirname + '/2022-02/peratusan.json');
+});
+
+app.get('/statistik', (req, res) => {
+  res.sendFile(__dirname + '/2022-02/statistik.json');
+});
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
