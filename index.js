c0b1e6a1de90ccdacfea45b475dd85ea3811dd46
airil28 <airilnlmoksin@gmail.com>
2023-02-27 10:16:30 +0800

    update

--- index.js
+++ index.js
@@ -9,22 +9,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.use(express.static('public'));
-
 // app.get('/dun', (req, res) => {
 //   res.sendFile(__dirname + '/2022-02/dun.json');
 // });
 
 app.get('/pilihanraya', (req, res) => {
   res.sendFile(__dirname + '/pilihanraya.json');
-});
-
-app.get('/calondun', (req, res) => {
-  res.sendFile(__dirname + '/2022-02/calon_dun.json');
-});
-
-app.get('/calonparlimen', (req, res) => {
-  res.sendFile(__dirname + '/2022-02/calon_parlimen.json');
 });
 
 app.get('/parlimen', (req, res) => {
@@ -40,7 +30,20 @@ app.get('/peratusan', (req, res) => {
 });
 
 app.get('/statistik', (req, res) => {
-  res.sendFile(__dirname + '/2022-02/statistik.json');
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  });
+
+  const data = require('./2022-02/statistik.json');
+  res.write('data: ');
+  res.write(JSON.stringify(data));
+  res.write('\n\n');
+
+  req.on('close', () => {
+    res.end();
+  });
 });
 
 app.get('/negeri', (req, res) => {
@@ -48,26 +51,23 @@ app.get('/negeri', (req, res) => {
 });
 
 app.get('/negeri2', (req, res) => {
-  // Set headers for SSE
   res.writeHead(200, {
     'Content-Type': 'text/event-stream',
     'Cache-Control': 'no-cache',
     'Connection': 'keep-alive'
   });
 
-  // Read the JSON file and send each candidate as a separate SSE event
   const data = require('./negeri.json');
   res.write('data: ');
   res.write(JSON.stringify(data));
   res.write('\n\n');
 
-  // Handle client disconnect
   req.on('close', () => {
     res.end();
   });
 });
 
-app.get('/calonparlimensse', (req, res) => {
+app.get('/calonparlimen', (req, res) => {
   // Set headers for SSE
   res.writeHead(200, {
     'Content-Type': 'text/event-stream',
@@ -75,7 +75,6 @@ app.get('/calonparlimensse', (req, res) => {
     'Connection': 'keep-alive'
   });
 
-  // Read the JSON file and send each candidate as a separate SSE event
   const data = require('./2022-02/calon_parlimen.json');
   res.write('data: ');
   res.write(JSON.stringify(data));
@@ -87,7 +86,7 @@ app.get('/calonparlimensse', (req, res) => {
   });
 });
 
-app.get('/calondunsse', (req, res) => {
+app.get('/calondun', (req, res) => {
   // Set headers for SSE
   res.writeHead(200, {
     'Content-Type': 'text/event-stream',
@@ -95,7 +94,6 @@ app.get('/calondunsse', (req, res) => {
     'Connection': 'keep-alive'
   });
 
-  // Read the JSON file and send each candidate as a separate SSE event
   const data = require('./2022-02/calon_dun.json');
   res.write('data: ');
   res.write(JSON.stringify(data));
