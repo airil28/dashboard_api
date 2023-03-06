6dce696ee51b76f6d95fca22dbf0eef23fe78bbe
airil28 <airilnlmoksin@gmail.com>
2023-03-06 15:51:18 +0800

    UPDATE

--- index.js
+++ index.js
@@ -6,23 +6,20 @@ var cors = require('cors')
 app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Dashboard API');
 });
 
 app.get('/pilihanraya', (req, res) => {
   res.sendFile(__dirname + '/pilihanraya.json');
 });
 
-app.get('/parlimen', (req, res) => {
-  res.sendFile(__dirname + '/2022-02/parlimen.json');
-});
-
-app.get('/dun', (req, res) => {
-  res.sendFile(__dirname + '/2022-02/dun.json');
-});
-
 app.get('/negeri', (req, res) => {
   res.sendFile(__dirname + '/negeri.json');
+});
+
+// PRU KE 15
+app.get('/dun', (req, res) => {
+  res.sendFile(__dirname + '/2022-02/dun.json');
 });
 
 app.get('/calonparlimen', (req, res) => {
@@ -44,6 +41,33 @@ app.get('/statistik', (req, res) => {
 app.get('/dppr', (req, res) => {
   res.sendFile(__dirname + '/2022-02/DPPR.json');
 });
+
+app.get('/parlimen', (req, res) => {
+  res.sendFile(__dirname + '/parlimen.json');
+});
+
+// PRU KE 14
+
+app.get('/2018-01/calonparlimen', (req, res) => {
+  res.sendFile(__dirname + '/2018-01/calon_parlimen.json');
+});
+
+app.get('/2018-01/calondun', (req, res) => {
+  res.sendFile(__dirname + '/2022-02/calon_dun.json');
+});
+
+app.get('/2018-01/keputusan', (req, res) => {
+  res.sendFile(__dirname + '/2018-01/keputusan.json');
+});
+
+app.get('/2018-01/statistik', (req, res) => {
+  res.sendFile(__dirname + '/2018-01/statistik.json');
+});
+
+app.get('/2018-01/dppr', (req, res) => {
+  res.sendFile(__dirname + '/2018-01/DPPR.json');
+});
+
 
 app.get('/keputusan2', (req, res) => {
   res.writeHead(200, {
