4c2c3aa586f2e9903d64429e0cd97b2cc7c97f34
Muhamad Airil <airilnlmoksin@gmail.com>
2023-06-18 17:02:28 +0800

    data

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
-const express = require('express');
-const app = express();
-const port = 5051;
-const host = '0.0.0.0';
-var cors = require('cors')
+const express = require('express')
+const path = require('path')
 
-app.use(cors())
+const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Dashboard API');
-});
+app.use(express.static('/var/www/spr_integrated/public/api'))
 
-app.get('/pilihanraya', (req, res) => {
-  res.sendFile(__dirname + '/pilihanraya.json');
-});
+// SSE endpoint
+// app.get('/api/v1', (req, res) => {
+//   res.setHeader('Content-Type', 'text/event-stream')
+//   res.setHeader('Cache-Control', 'no-cache')
+//   res.setHeader('Connection', 'keep-alive')
 
-app.get('/negeri', (req, res) => {
-  res.sendFile(__dirname + '/negeri.json');
-});
+//   // Send SSE messages periodically
+//   const intervalId = setInterval(() => {
+//     const data = 'This is an SSE message'
+
+//     // Send SSE event
+//     res.write(`event: message\n`)
+//     res.write(`data: ${data}\n\n`)
+//   }, 1000);
+
+//   // Close SSE connection on client disconnect
+//   req.on('close', () => {
+//     clearInterval(intervalId)
+//     res.end()
+//   })
+// })
 
 
-//////////////// PRU KE 15
-app.get('/2022-02/dun', (req, res) => {
-  res.sendFile(__dirname + '/2022-02/dun.json');
-});
-
-app.get('/2022-02/calonparlimen', (req, res) => {
+app.get('/api/v1/:kodPilihanraya/calondun', (req, res) => {
   // Set headers for SSE
   res.writeHead(200, {
     'Content-Type': 'text/event-stream',
     'Cache-Control': 'no-cache',
     'Connection': 'keep-alive'
   });
-
-  const data = require('./2022-02/calon_parlimen.json');
+  const kodPilihanraya = req.params.kodPilihanraya;
+  const data = require('/var/www/spr_integrated/public/api/${kodPilihanraya}/calondun.json');
   res.write('data: ');
   res.write(JSON.stringify(data));
   res.write('\n\n');
@@ -43,328 +47,22 @@ app.get('/2022-02/calonparlimen', (req, res) => {
   });
 });
 
-app.get('/2022-02/calondun', (req, res) => {
-  // Set headers for SSE
-  res.writeHead(200, {
-    'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive'
-  });
+app.get('/example', (req, res) => {
+  // Example JSON response
+  const responseBody = {
+    message: 'This is an example response.',
+    data: {
+      foo: 'bar',
+      baz: 123,
+    },
+  };
 
-  const data = require('./2022-02/calon_dun.json');
-  res.write('data: ');
-  res.write(JSON.stringify(data));
-  res.write('\n\n');
-
-  // Handle client disconnect
-  req.on('close', () => {
-    res.end();
-  });
+  res.json(responseBody);
 });
 
-app.get('/2022-02/keputusan', (req, res) => {
-  res.sendFile(__dirname + '/2022-02/keputusan.json');
-});
-
-app.get('/2022-02/dppr', (req, res) => {
-  res.sendFile(__dirname + '/2022-02/DPPR.json');
-});
-
-app.get('/2022-02/parlimen', (req, res) => {
-  res.sendFile(__dirname + '/parlimen.json');
-});
-
-app.get('/2022-02/statistikumur', (req, res) => {
-  res.sendFile(__dirname + '/2022-02/statistikumur.json');
-});
-
-app.get('/2022-02/statistikjenis', (req, res) => {
-  res.sendFile(__dirname + '/2022-02/statistikjenis.json');
-});
-
-//PRU DUN JOHOR KE-15------------------------------------
-app.get('/2022-01/calondun', (req, res) => {
-  // Set headers for SSE
-  res.writeHead(200, {
-    'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive'
-  });
-
-
-  const data = require('./2022-01/calon_dun.json');
-  res.write('data: ');
-  res.write(JSON.stringify(data));
-  res.write('\n\n');
-
-  // Handle client disconnect
-  req.on('close', () => {
-    res.end();
-  });
-});
-
-app.get('/2022-01/statistikumur', (req, res) => {
-  res.sendFile(__dirname + '/2022-01/statistikumur.json');
-});
-
-app.get('/2022-01/statistikjenis', (req, res) => {
-  res.sendFile(__dirname + '/2022-01/statistikjenis.json');
-});
-
-app.get('/2022-01/keputusan', (req, res) => {
-  res.sendFile(__dirname + '/2022-01/keputusan.json');
-});
-
-app.get('/2022-01/dppr', (req, res) => {
-  res.sendFile(__dirname + '/2022-01/DPPR.json');
-});
-
-app.get('/2022-01/statistik', (req, res) => {
-  // Set headers for SSE
-  res.writeHead(200, {
-    'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive'
-  });
-
-  const data = require('./2022-01/statistik.json');
-  res.write('data: ');
-  res.write(JSON.stringify(data));
-  res.write('\n\n');
-
-  // Handle client disconnect
-  req.on('close', () => {
-    res.end();
-  });
-});
-
-//PRK TIOMAN-----------------------------------------
-
-app.get('/2022-05/calondun', (req, res) => {
-  // Set headers for SSE
-  res.writeHead(200, {
-    'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive'
-  });
-
-
-  const data = require('./2022-05/calon_dun.json');
-  res.write('data: ');
-  res.write(JSON.stringify(data));
-  res.write('\n\n');
-
-  // Handle client disconnect
-  req.on('close', () => {
-    res.end();
-  });
-});
-
-app.get('/2022-05/keputusan', (req, res) => {
-  res.sendFile(__dirname + '/2022-05/keputusan.json');
-});
-
-app.get('/2022-05/dppr', (req, res) => {
-  res.sendFile(__dirname + '/2022-05/DPPR.json');
-});
-
-app.get('/2022-05/parlimen', (req, res) => {
-  res.sendFile(__dirname + '/parlimen.json');
-});
-
-app.get('/2022-05/statistikumur', (req, res) => {
-  res.sendFile(__dirname + '/2022-05/statistikumur.json');
-});
-
-app.get('/2022-05/statistikjenis', (req, res) => {
-  res.sendFile(__dirname + '/2022-05/statistikjenis.json');
-});
-
-//PRK padang serai-----------------------------------------
-
-app.get('/2022-04/calonparlimen', (req, res) => {
-  // Set headers for SSE
-  res.writeHead(200, {
-    'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive'
-  });
-
-
-  const data = require('./2022-04/calon_parlimen.json');
-  res.write('data: ');
-  res.write(JSON.stringify(data));
-  res.write('\n\n');
-
-  // Handle client disconnect
-  req.on('close', () => {
-    res.end();
-  });
-});
-
-app.get('/2022-04/keputusan', (req, res) => {
-  res.sendFile(__dirname + '/2022-04/keputusan.json');
-});
-
-app.get('/2022-04/dppr', (req, res) => {
-  res.sendFile(__dirname + '/2022-04/DPPR.json');
-});
-
-app.get('/2022-04/parlimen', (req, res) => {
-  res.sendFile(__dirname + '/parlimen.json');
-});
-
-app.get('/2022-04/statistikumur', (req, res) => {
-  res.sendFile(__dirname + '/2022-05/statistikumur.json');
-});
-
-app.get('/2022-04/statistikjenis', (req, res) => {
-  res.sendFile(__dirname + '/2022-05/statistikjenis.json');
-});
-
-// TEST-----------------------------------------------
-
-app.get('/2023-20/calondun', (req, res) => {
-  // Set headers for SSE
-  res.writeHead(200, {
-    'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive'
-  });
-
-
-  const data = require('./2023-20/calon_dun.json');
-  res.write('data: ');
-  res.write(JSON.stringify(data));
-  res.write('\n\n');
-
-  // Handle client disconnect
-  req.on('close', () => {
-    res.end();
-  });
-});
-
-app.get('/keputusan2', (req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive'
-  });
-
-  const data = require('./2022-02/keputusan.json');
-  res.write('data: ');
-  res.write(JSON.stringify(data));
-  res.write('\n\n');
-
-  req.on('close', () => {
-    res.end();
-  });
-});
-
-app.get('/statistik2', (req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive'
-  });
-
-  const data = require('./2022-02/statistik.json');
-  res.write('data: ');
-  res.write(JSON.stringify(data));
-  res.write('\n\n');
-
-  req.on('close', () => {
-    res.end();
-  });
-});
-
-
-
-app.get('/negeri2', (req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive'
-  });
-
-  const data = require('./negeri.json');
-  res.write('data: ');
-  res.write(JSON.stringify(data));
-  res.write('\n\n');
-
-  req.on('close', () => {
-    res.end();
-  });
-});
-
-app.get('/calonparlimen2', (req, res) => {
-  // Set headers for SSE
-  res.writeHead(200, {
-    'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive'
-  });
-
-  const data = require('./2022-02/calon_parlimen.json');
-  res.write('data: ');
-  res.write(JSON.stringify(data));
-  res.write('\n\n');
-
-  // Handle client disconnect
-  req.on('close', () => {
-    res.end();
-  });
-});
-
-app.get('/calondun2', (req, res) => {
-  // Set headers for SSE
-  res.writeHead(200, {
-    'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive'
-  });
-
-  const data = require('./2022-02/calon_dun.json');
-  res.write('data: ');
-  res.write(JSON.stringify(data));
-  res.write('\n\n');
-
-  // Handle client disconnect
-  req.on('close', () => {
-    res.end();
-  });
-});
-
-//pru dun selangor
-
-app.get('/2023-38/calondun', (req, res) => {
-  // Set headers for SSE
-  res.writeHead(200, {
-    'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive'
-  });
-
-
-  const data = require('./2023-38/calon_dun.json');
-  res.write('data: ');
-  res.write(JSON.stringify(data));
-  res.write('\n\n');
-
-  // Handle client disconnect
-  req.on('close', () => {
-    res.end();
-  });
-});
-
-app.get('/2023-38/dun', (req, res) => {
-  res.sendFile(__dirname + '/2023-38/dun.json');
-});
-
-app.get('/2023-38/keputusan', (req, res) => {
-  res.sendFile(__dirname + '/2023-38/keputusan.json');
-});
-
+// Start the server
+const port = 5051
+const host = '0.0.0.0' // Specify the server's public IP address or domain name
 app.listen(port, host, () => {
-  console.log(`Server running at http://${host}:${port}`);
-});
+  console.log(`Server is running on ${host}:${port}`)
+})
