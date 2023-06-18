7b74d6e938dd1ec23eceb0e9c5bf2aff8ed8333a
Muhamad Airil <airilnlmoksin@gmail.com>
2023-06-18 18:21:26 +0800

    serve

--- index.js
+++ index.js
@@ -29,10 +29,74 @@ app.get('/api/v1/:kodPilihanraya/calondun', (req, res) => {
   });
 });
 
+app.get('/api/v1/:kodPilihanraya/calonparlimen', (req, res) => {
+  // Set headers for SSE
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  });
+  const kodPilihanraya = req.params.kodPilihanraya;
+  const data = require('/var/www/spr_integrated/public/api/'+kodPilihanraya+'/calonparlimen.json');
+
+  res.write('data: ');
+  res.write(JSON.stringify(data));
+  res.write('\n\n');
+  // Handle client disconnect
+  req.on('close', () => {
+    res.end();
+  });
+});
+
+app.get('/api/v1/:kodPilihanraya/keputusan', (req, res) => {
+  // Set headers for SSE
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  });
+  const kodPilihanraya = req.params.kodPilihanraya;
+  const data = require('/var/www/spr_integrated/public/api/'+kodPilihanraya+'/keputusan.json');
+
+  res.write('data: ');
+  res.write(JSON.stringify(data));
+  res.write('\n\n');
+  // Handle client disconnect
+  req.on('close', () => {
+    res.end();
+  });
+});
+
+app.get('/api/v1/pilihanraya', (req, res) => {
+  res.sendFile('/var/www/spr_integrated/public/api/pilihanraya.json');
+});
+
+app.get('/api/v1/:kodPilihanraya/parlimen', (req, res) => {
+  res.sendFile('/var/www/spr_integrated/public/api/'+kodPilihanraya+'/parlimen.json');
+});
+
+app.get('/api/v1/:kodPilihanraya/dun', (req, res) => {
+  res.sendFile('/var/www/spr_integrated/public/api/'+kodPilihanraya+'/dun.json');
+});
+
+app.get('/api/v1/:kodPilihanraya/statistikumur', (req, res) => {
+  res.sendFile('/var/www/spr_integrated/public/api/'+kodPilihanraya+'/statistikumur.json');
+});
+
+app.get('/api/v1/:kodPilihanraya/statistikjenis', (req, res) => {
+  res.sendFile('/var/www/spr_integrated/public/api/'+kodPilihanraya+'/statistikjenis.json');
+});
+
+app.get('/api/v1/:kodPilihanraya/dppr', (req, res) => {
+  res.sendFile('/var/www/spr_integrated/public/api/'+kodPilihanraya+'/dppr.json');
+});
+
+
+
 app.get('/example/:data', (req, res) => {
   // Example JSON response
   const data = req.params.data;
-  const example = require('/var/www/spr_integrated/public/api/'+data+'/calondun');
+  const example = require('/var/www/spr_integrated/public/api/'+data+'/calondun.json');
   const responseBody = {
     message: 'This is an example response.${example}',
     data: {
@@ -50,3 +114,4 @@ const host = '0.0.0.0' // Specify the server's public IP address or domain name
 app.listen(port, host, () => {
   console.log(`Server is running on ${host}:${port}`)
 })
+
